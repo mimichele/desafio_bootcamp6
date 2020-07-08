b1a41eff4d94d60c5930bd2ad6440902dbcc7011
Michele Matumoto <michele.matumoto@smartbpo.io>
2020-07-07 21:49:36 -0300

    Desafio Bootcamp 06

---
+++ src/services/DeleteTransactionService.ts
@@ -0,0 +1,23 @@
+import AppError from '../errors/AppError';
+import { getCustomRepository} from 'typeorm';
+
+import Transaction from '../models/Transaction';
+
+import Transactionsrepository from '../repositories/TransactionsRepository';
+
+class DeleteTransactionService {
+  public async execute(id: string): Promise<void> {
+    // TODO
+    const transactionRepository = getCustomRepository(Transactionsrepository);
+
+    const transaction = await transactionRepository.findOne(id);
+
+    if(!transaction){
+      throw new AppError('Transaction does not exist');
+    }
+
+    await transactionRepository.remove(transaction);
+  }
+}
+
+export default DeleteTransactionService;
